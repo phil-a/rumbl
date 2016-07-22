88bc6d77bf639042d6c5840bf55326de5c6f39f0
Phil Alekseev <philalekseev@administorsmbp5.corp.mosaic.com>
2016-07-22 09:53:33 -0400

    add new User creation

--- web/models/user.ex
+++ web/models/user.ex
@@ -9,4 +9,10 @@ defmodule Rumbl.User do
 
     timestamps
   end
+
+  def changeset(model, params \\ :empty) do
+    model
+    |> cast(params, ~w(name username), []) #check required fields and cast to schema types
+    |> validate_length(:username, min: 1, max: 20)
+  end
 end
