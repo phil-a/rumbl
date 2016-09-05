9acf8775e8337abe19313f5166217bd4e3dfa3c0
Philip Alekseev <phil.alekseev@gmail.com>
2016-09-05 15:38:20 -0400

    Create Slugs for Videos

--- web/models/video.ex
+++ web/models/video.ex
@@ -1,12 +1,14 @@
 defmodule Rumbl.Video do
   use Rumbl.Web, :model
-
+  require IEx
+  @primary_key {:id, Rumbl.Permalink, autogenerate: true}
   schema "videos" do
     field :url, :string
     field :title, :string
     field :description, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
+    field :slug, :string
 
     timestamps()
   end
@@ -18,6 +20,30 @@ defmodule Rumbl.Video do
     struct
     |> cast(params, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
   end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(str) do
+    str
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+
+  # implement Phoenix.Param protocol
+  # clean polymorphism - extended without changing Phoenix or Video module
+  defimpl Phoenix.Param, for: Rumbl.Video do
+    def to_param(%{slug: slug, id: id}) do
+      "#{id}-#{slug}"
+    end
+  end
+
 end
